b8da4278466649a179b3a4a98caac9cb397fc392
Domenick <152310637+ddddsfsgf@users.noreply.github.com>
2024-02-13 15:21:10 -0500

    Add files via upload

---
+++ Fruit_Search.js
@@ -0,0 +1,25 @@
+let fruits = ['Apple', 'Apricot', 'Avocado', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu']
+const input = document.querySelector("#fruit");
+const suggestions = document.querySelector(".suggestions ul")
+input.addEventListener('keyup', () => {
+	let results =[];
+	let searched = input.value;
+	if(searched.length){
+		results = fruits.filter((item) => {
+			return item.toLowerCase().includes(searched.toLowerCase());
+		});
+	}
+	showSuggestions(results);
+});
+function showSuggestions(results){
+	const inputVal = results.map((item) => {
+		return `<li onclick=useSuggestion(this)>${item}</li>`;
+	})
+	suggestions.classList.add("show");
+	suggestions.innerHTML = inputVal.join('');
+}
+function useSuggestion(item){
+	input.value = item.innerHTML;
+	suggestions.innerHTML = '';
+}
+
